5a2d7368ebf03199e491528b541abcb6afa83f33
go367it <satyamkumar3001@gmail.com>
2022-07-14 09:49:05 +0530

    added data

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,42 +1,35 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Tooltip from "@mui/material/Tooltip";
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
-export default function DetailsCard() {
+export default function DetailsCard(props) {
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card variant="outlined" sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
         <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
+          {props.title}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+          {props.platform}
         </Typography>
         <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          <Stack direction="row" spacing={1}>
+            <Tooltip title="Genre of the game" placement="top" arrow>
+              <Chip label={props.genre} />
+            </Tooltip>
+            <Tooltip title="Overall Score" placement="top" arrow>
+              <Chip label={props.score} />
+            </Tooltip>
+            <Tooltip title="Editors choice" placement="top" arrow>
+              <Chip label={props.editors_choice} />
+            </Tooltip>
+          </Stack>
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DetailsCard from "../components/DetailsCard";
+import Grid from "@mui/material/Grid";
+import { Paper } from "@mui/material";
 
 const Homepage = () => {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     const config = {
       url: `https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json`,
@@ -10,7 +14,8 @@ const Homepage = () => {
     };
     axios(config)
       .then((res) => {
-        console.log(res);
+        console.log(res.data.slice(1, res.data.length));
+        setData(res.data.slice(1, res.data.length));
       })
       .catch((err) => {
         console.log(err);
@@ -18,9 +23,29 @@ const Homepage = () => {
   }, []);
 
   return (
-    <div>
-      <DetailsCard />
-    </div>
+    <Paper
+      variant="outlined"
+      sx={{
+        padding: "10px",
+      }}
+    >
+      <Grid container spacing={2}>
+        {data.map((j) => {
+          return (
+            <Grid key={j.id} item xs={4}>
+              <DetailsCard 
+              key={j.id} 
+              title={j.title} 
+              platform={j.platform}
+              genre={j.genre}
+              score={j.score}
+              editors_choice={j.editors_choice}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Paper>
   );
 };
 
